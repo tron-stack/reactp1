4313c70ead0f713008f543403fb79b47a17c9e91
cpalden <chimepalden@gmail.com>
2022-05-19 16:45:30 -0400

    edited reimbursement page

--- src/Views/ReimbursementPage/ReimbursementPage.tsx
+++ src/Views/ReimbursementPage/ReimbursementPage.tsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "../../UserStore";
 import { useNavigate } from "react-router-dom";
-import { getReimbursments } from "../../Slices/ReimbursementSlice";
+import { getReimbursments, getReimbursmentsForManager } from "../../Slices/ReimbursementSlice";
 import { Loading } from "../../Components/Loading/Loading";
 import { Reimbursement } from "../../Components/Reimbursements/Reimbursements";
 import { IReimbursement } from "../../Interfaces/IReimbursement";
@@ -16,11 +16,18 @@ export const ReimbursementPage:React.FC = () => {
     const navigator = useNavigate();
     const dispatch:AppDispatch = useDispatch();
 
+    // get username or userid for manager, request status for user
+
     useEffect(()=>{
         if(!userInfo.user){
             navigator('/login');
         } else if(userInfo.user && !reimbursementsInfo.reimbursements) {
-            dispatch(getReimbursments());
+            if(userInfo.user.userRole == 1){
+                dispatch(getReimbursments());
+            }else if(userInfo.user.userRole == 2){
+                dispatch(getReimbursmentsForManager())
+            } 
+                
         }
         console.log("UserState: ", userInfo, "Reimbursements: ", reimbursementsInfo);
 
@@ -33,7 +40,7 @@ export const ReimbursementPage:React.FC = () => {
                     return <Reimbursement {...reimbursement} key={reimbursement.reimbursementId} />
             }):
             <Loading/>
-        }
+            }
         </div>
     )
 
